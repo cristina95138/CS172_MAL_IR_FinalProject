50f35037dff72040d885d5d32a6cd75b930ae162
Cristina Lawson <30380172+cristina95138@users.noreply.github.com>
2021-06-10 23:12:47 -0700

    Retrieval System

--- retrievalSystem.py
+++ retrievalSystem.py
@@ -6,25 +6,6 @@ with open('output.csv') as f:
 	reader = csv.DictReader(f)
 	helpers.bulk(es, reader, index='animes')
 
-doc = {
-	    "settings" : {
-	        "number_of_shards": 5,
-	        "number_of_replicas": 1
-	    },
-
-	    'mappings': {
-	        'examplecase': {
-	            'properties': {
-	                'anime': {'index': 'not_analyzed', 'type': 'string'},
-	                'rating': {'index': 'not_analyzed', 'type': 'string'},
-	                'description': {'index': 'not_analyzed', 'type': 'string'},
-	                'rank': {'index': 'analyzed', 'type': 'string'},
-	                'episodes': {'index': 'not_analyzed', 'type': 'string'},
-	            }}}
-	}
-
-index = es.indices.create(index='mal_index', body=doc)
-
 result = es.search(
           index="contents",
           body={
